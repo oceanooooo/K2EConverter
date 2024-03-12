3c64104aa5b53c531569387d4762f69bf3750102
oceanooooo <funboard6287@gmail.com>
2024-03-12 22:00:36 +0900

    [feat] 영어를 한글 문자로 변환

---
+++ K2EConverter/K2EConverter/K2EConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  K2EConverterApp.swift
+//  K2EConverter
+//
+//  Created by OhSuhyun on 2024.03.12.
+//
+
+import SwiftUI
+
+@main
+struct K2EConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
